490b07e017f4667543cda58f542ab3822a4f0a57
jslsxu <jslsxu@163.com>
2017-06-27 15:06:15 +0800

    no message

---
+++ HaocaiUtil/Classes/Util.h
@@ -0,0 +1,13 @@
+//
+//  Util.h
+//  Pods
+//
+//  Created by jslsxu on 2017/6/27.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Util : NSObject
++ (void)print;
+@end
---
+++ HaocaiUtil/Classes/Util.m
@@ -0,0 +1,15 @@
+//
+//  Util.m
+//  Pods
+//
+//  Created by jslsxu on 2017/6/27.
+//
+//
+
+#import "Util.h"
+
+@implementation Util
++ (void)print{
+    NSLog(@"util print");
+}
+@end
